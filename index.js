a2b01f4edf1e2c8c13072fbbd69adce6bc8b184c
Sofia-Bourdon <112895499+Sofia-Bourdon@users.noreply.github.com>
2024-10-22 11:35:16 +0200

    Ajout webpacj

--- index.js
+++ index.js
@@ -23,11 +23,24 @@ const getFactures = async (db) => {
     return factures;
 }
 
+const afficheFactures = (factures) => {
+    const rootE1 = document.querySelector('#root');
+    const ulE1 = document.createElement('ul');
+    factures.map(factures => {
+        const liE1 = document.createElement("li");
+        liE1.innerHTML = facture.id + " <button classe='deleteFacture' data-id="+facture.id">x<button>";
+        ulE1/appendChild(liE1);
+    });
+
+    const buttonsDelete = document.querySelectorAll('.deleteFacture');
+    buttonsDelete.forEach(button => {
+        button.addEventListener('', () => {
+            console.log("click");
+            console.log(event.target.getAttribute('data-id'));
+        });
+    });
+
+    rootE1.appendChild(ulE1);
+}
+
 const factures = await getFactures(db)
-
-factures.forEach(factures => {
-
-    if (isNaN(facture.totalTTC) || && parseFloat(facture.totalTTC) > -10) {
-        console.log(facture.id);
-    }
-});
